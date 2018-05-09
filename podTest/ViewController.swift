3b954de6cafc2db6620313241fdad1a9bb8edc1e
Varun macmini10 <Varun@cogzidel.com>
2018-05-09 10:54:05 +0530

    added 3 pods

---
+++ podTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  podTest
+//
+//  Created by apple on 09/05/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
